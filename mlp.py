45b68d15504581e8edff4eec599e3f38c5825f53
Hauke Dau <4dau@informatik.uni-hamburg.de>
2017-10-27 16:43:14 +0200

    MLP Complete

--- mlp.py
+++ mlp.py
@@ -11,41 +11,58 @@ import numpy as numpy
 input    = numpy.array([[0,0],[0,1],[1,0],[1,1]])
 expected = numpy.array([[0],[1],[1],[0]])
 
-trainingRuns= 20
+trainingRuns= 5000
 hiddenLayerNeuronen = 10
 inputDimension = 2
 outputDimension = 1
-lernRate = 0.1
+learnRate = 0.1
 
 weightsInputToHiddenLayer = numpy.random.uniform(-0.1,0.1,(hiddenLayerNeuronen,inputDimension))
 weightsHiddenToOutputLayer = numpy.random.uniform(-0.1,0.1,(outputDimension,hiddenLayerNeuronen))
+
+biasHidden = numpy.random.uniform(-0.1,0.1,(10))
+biasOut = numpy.random.uniform(-0.1,0.1,(1))
 
 for trainingRun in range(trainingRuns):
     for i in range(4):
         #Feedforward
         currentInput = numpy.copy(input[i])
-        inputHiddenLayer = numpy.dot(weightsInputToHiddenLayer,currentInput)  
+        inputHiddenLayer = numpy.dot(weightsInputToHiddenLayer,currentInput) + biasHidden
         #print ("inputHiddenLayer") 
         #print (inputHiddenLayer)
         outputHiddenLayer = 1/(1+numpy.exp(-inputHiddenLayer)) 
-        print ("outputHiddenLayer") 
-        print(outputHiddenLayer)
-        OutputLayer = numpy.dot(weightsHiddenToOutputLayer,outputHiddenLayer)
+        #print ("outputHiddenLayer") 
+        #print(outputHiddenLayer)
+        outputLayer = numpy.dot(weightsHiddenToOutputLayer,outputHiddenLayer) + biasOut
         #print("OutputLayer")
         #print(OutputLayer)
         
         #backprobab        
-        error = expected[i] - OutputLayer
+        error = expected[i] - outputLayer
         derivation = outputHiddenLayer*(1-outputHiddenLayer)       
-        print("derivation")
-        print(derivation)
-        deltaHiddenLayer = derivation*numpy.dot(numpy.transpose(OutputLayer),error)
+        #print("derivation")
+        #print(derivation)
+        deltaHiddenLayer = derivation*numpy.dot(numpy.transpose(outputLayer),error)
         
         #learning
+        weightsHiddenToOutputLayer += learnRate * numpy.outer(error,outputHiddenLayer)
+        weightsInputToHiddenLayer += learnRate * numpy.outer(deltaHiddenLayer, currentInput)
+        
+        biasOut += learnRate * error
+        biasHidden += learnRate * deltaHiddenLayer
         
         
+        #print("weightsHiddenToOutputLayer")
+        #print(weightsHiddenToOutputLayer)        
+        
+        #print("weightsInputToHiddenLayer")
+        #print(weightsInputToHiddenLayer)
+
+        #if i == 0:
+        print("error")
+        print(error)
 
 #print(weightsInputToHiddenLayer)
 
-print(input)
-print(expected)
+#print(input)
+#print(expected)
